ef13503491715b1e7523fcf61e4d0c2f998bd7be
Lyea Paul <104055190+lyeapp@users.noreply.github.com>
2024-08-19 17:24:42 -0400

    Update

--- Toy-shop/backend/server.js
+++ Toy-shop/backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DBHOST, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
